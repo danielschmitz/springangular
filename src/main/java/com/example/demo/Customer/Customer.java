cd7e6737e9b7c62480df993bea817e984071b8e8
Daniel Schmitz <danieljfa@gmail.com>
2021-10-20 22:25:33 -0300

    missing file

---
+++ src/main/java/com/example/demo/Customer/Customer.java
@@ -0,0 +1,43 @@
+package com.example.demo.Customer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer() {
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + id + "'" +
+            ", firstName='" + firstName + "'" +
+            ", lastName='" + lastName + "'" +
+            "}";
+    }
+    
+
+}
